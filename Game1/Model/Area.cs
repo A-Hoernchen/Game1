3353b30d260b161744b42a3f5df94365e52f2dc6
Aaron Bostelmann <bostelmann@bitsign.de>
2017-10-11 15:15:38 +0200

    World

---
+++ Game1/Model/Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Model
+{
+	public class Area
+	{
+		public List<Tile> Tiles
+		{
+			get;
+			private set;
+		}
+		public Area()
+		{
+			Tiles = new List<Tile>();
+		}
+	}
+}
---
+++ Game1/Model/World.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Model
+{
+	public class World
+	{
+		public List<Area> Areas
+		{
+			get;
+			private set;
+		}
+		public World()
+		{
+			Areas = new List<Area>();
+		}
+	}
+}
